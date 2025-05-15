1bf184da858f82d6f4f10ad4e720b81c1762c85d
Shachiru <shachirurashmika35@gmail.com>
2025-05-15 11:02:50 +0530

    eureka server, product-service and order-service modules

--- product-service/src/main/java/lk/ijse/productservice/controller/ProductController.java
+++ product-service/src/main/java/lk/ijse/productservice/controller/ProductController.java
@@ -1,4 +1,17 @@
 package lk.ijse.productservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/product")
 public class ProductController {
+
+    @GetMapping("all")
+    public String getAll(){
+        return "pr 1, pr 2, pr 3";
+    }
 }
+
+//  GET - http://localhost:8081/product-service/api/v1/product/all
